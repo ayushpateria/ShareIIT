b46b2482d913a97ad5c5f74e7110b48bf21250c9
Shikhar Agrawal <Shikhar Agrawal>
2017-02-28 02:07:58 +0530

    jdkaf

--- test.go
+++ test.go
@@ -4,11 +4,25 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"log"
 )
 
 func list(){
 	filepath.Walk("Shared", func(path string, info os.FileInfo, err error) error {
-		fmt.Println(path)
+		//fmt.Println(path)
+			file, err := os.Open(path)
+			fileInfo, err := file.Stat()
+			if err != nil {
+				log.Fatal(err)
+			}
+			//fmt.Println(fi.Size())
+			fmt.Println("File name:", fileInfo.Name())
+			fmt.Println("Size in bytes:", fileInfo.Size())
+			fmt.Println("Permissions:", fileInfo.Mode())
+			//fmt.Println("Last modified:", fileInfo.ModTime())
+			//fmt.Println("Is Directory: ", fileInfo.IsDir())
+			//fmt.Printf("System interface type: %T\n", fileInfo.Sys())
+			//fmt.Printf("System info: %+v\n\n", fileInfo.Sys())
 		return nil
 	})
 }
