8b780ddbbc268d1587b89bee06a4d03358b9c9b9
Ayush Pateria <cs15btech11007@iith.ac.in>
2017-03-07 01:27:14 +0530

    Change gitignore

--- client.go
+++ client.go
@@ -1,170 +1,172 @@
-
 package main
 
 import (
-	"fmt"
-	"net"
-	"os"
-    "bufio"
-    "io"
-    "strconv"
-    "net/http"
-	"strings"
-	"io/ioutil"
-	"sync"
+	"bufio"
 	"encoding/json"
-	)
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
+	"sync"
+)
 
 type File_s struct {
 	Name string
 	Hash string
-	Size float64	
- 	path string
-	ip string
+	Size float64
+	path string
+	ip   string
 }
 
-var files []File_s 
+var files []File_s
 var ips []string
 var LIST_URL = "http://ayushpateria.com/ShareIIT/list.php"
 
 //Define that the binairy data of the file will be sent 1024 bytes at a time
 const BUFFERSIZE = 81920
 
-
 func fetchIPS() {
-		response, err := http.Get(LIST_URL)
-        if err != nil {
-                fmt.Println(err)
-        } else {
-                defer response.Body.Close()
-				r,_ := ioutil.ReadAll(response.Body)
-                json.Unmarshal(r, &ips)
-                if err != nil {
-                        fmt.Println(err)
-                }
-        }
+	ips = nil
+	response, err := http.Get(LIST_URL)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		defer response.Body.Close()
+		r, _ := ioutil.ReadAll(response.Body)
+		json.Unmarshal(r, &ips)
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
 
 }
 
 func updateList(ip string) {
-	var lFiles []File_s 
+	var lFiles []File_s
 	connection, err := net.Dial("tcp", ip+":3333")
 	if err != nil {
 		panic(err)
-}
+	}
 	defer connection.Close()
-	fmt.Fprintf(connection, "1\n")	
+	fmt.Fprintf(connection, "1\n")
 	message, _ := bufio.NewReader(connection).ReadString('\n')
 
 	json.Unmarshal([]byte(message), &lFiles)
-	for i,_ := range(lFiles) {
+	for i, _ := range lFiles {
 		lFiles[i].ip = ip
 	}
 	files = append(files, lFiles...)
 }
 
 func createList() {
-		files = nil
-		fetchIPS()
-		var wg sync.WaitGroup
-		for _, ip := range ips {
-			// Increment the WaitGroup counter.
-                wg.Add(1)
-			go func(ip string) {
-                        // Decrement the counter when the goroutine completes.
-                       defer wg.Done()
-                        // Fetch the URL.
-                        updateList(ip)
-                }(ip)
-		}
+	files = nil
+	fetchIPS()
+	var wg sync.WaitGroup
+	for _, ip := range ips {
+		// Increment the WaitGroup counter.
+		wg.Add(1)
+		go func(ip string) {
+			// Decrement the counter when the goroutine completes.
+			defer wg.Done()
+			// Fetch the URL.
+			updateList(ip)
+		}(ip)
+	}
 	// Wait for all Lists fetches to complete.
-        wg.Wait()
+	wg.Wait()
 }
 
 func main() {
 
 	fmt.Println("Welcome to ShareIIT! Your intra college file sharing hub.")
-	for {
+
 	fmt.Println("1. List all available files.")
 	fmt.Println("2. Download a file,")
 	fmt.Println("3. Search a file.")
 	fmt.Println("0. Exit.")
-	
-	// read in input from stdin
-    reader := bufio.NewReader(os.Stdin)
-    option, _ := reader.ReadString('\n')
-    if option[:1] == "1" {
-		
-		createList()
+	for {
 
-	    for  i, value := range files {
-	    	fmt.Print((i+1))
-	    	fmt.Print(". "+value.Name+"		")
-	    	fmt.Print(value.Size)
-	    	fmt.Println(" kb")
-	    	}
+		fmt.Print(">> ")
 
-	}else if option[:1] == "2"{
+		// read in input from stdin
+		reader := bufio.NewReader(os.Stdin)
+		option, _ := reader.ReadString('\n')
+		if option[:1] == "1" {
 
-				if (len(files) == 0) {
-					createList()
-				}
-				fmt.Println("Enter the ID of the file from the list : ")
-				var id int
-    			fmt.Scanf("%d", &id)
-				if (id > len(files)) {
-					fmt.Println("Please enter a valid ID.")
-				} else {
-    			recivefile(id-1)
-				}
-    		} else if option[:1] == "0"{
-				break
+			createList()
+
+			for i, value := range files {
+				fmt.Print((i + 1))
+				fmt.Print(". " + value.Name + "		")
+				fmt.Print(value.Size)
+				fmt.Println(" kb")
 			}
+
+		} else if option[:1] == "2" {
+
+			if len(files) == 0 {
+				createList()
+			}
+			fmt.Println("Enter the ID of the file from the list : ")
+			var id int
+			fmt.Scanf("%d", &id)
+			if id > len(files) {
+				fmt.Println("Please enter a valid ID.")
+			} else {
+				recivefile(id - 1)
+			}
+		} else if option[:1] == "0" {
+			break
+		}
 	}
 
-	}
-	
+}
+
 func recivefile(i int) {
-			file := files[i]
-			fmt.Println("Downloading " + file.Name + ", this may take a while.")
-			connection, err := net.Dial("tcp", file.ip+":3333")
-			if err != nil {
-				panic(err)
-			}
-			defer connection.Close()
-			hash := files[i].Hash
-    		fmt.Fprintf(connection, "2 "+hash + "\n")
-			//Create buffer to read in the name and size of the file
-			bufferFileName := make([]byte, 128)
-			bufferFileSize := make([]byte, 20)
-			//Get the filesize
-			connection.Read(bufferFileSize)
-			//Strip the ':' from the received size, convert it to a int64
-			fileSize, _ := strconv.ParseInt(strings.Trim(string(bufferFileSize), ":"), 20, 64)
-			//Get the filename
-			connection.Read(bufferFileName)
-			//Strip the ':' once again but from the received file name now
-			fileName := strings.Trim(string(bufferFileName), ":")
-			//Create a new file to write in
-			newFile, err := os.Create(fileName)
-			if err != nil {
-				panic(err)
-			}
-			defer newFile.Close()
-			//Create a variable to store in the total amount of data that we received already
-			var receivedBytes int64
-			//Start writing in the file
-			for {
-				if (fileSize - receivedBytes) < BUFFERSIZE {
-					io.CopyN(newFile, connection, (fileSize - receivedBytes))
-					//Empty the remaining bytes that we don't need from the network buffer
-					connection.Read(make([]byte, (receivedBytes+BUFFERSIZE)-fileSize))
-					//We are done writing the file, break out of the loop
-					break
-				}
-				io.CopyN(newFile, connection, BUFFERSIZE)
-				//Increment the counter
-				receivedBytes += BUFFERSIZE
-			}
-			fmt.Println("Received file completely!")
+	file := files[i]
+	fmt.Println("Downloading " + file.Name + ", this may take a while.")
+	connection, err := net.Dial("tcp", file.ip+":3333")
+	if err != nil {
+		panic(err)
 	}
+	defer connection.Close()
+	hash := files[i].Hash
+	fmt.Fprintf(connection, "2 "+hash+"\n")
+	//Create buffer to read in the name and size of the file
+	bufferFileName := make([]byte, 128)
+	bufferFileSize := make([]byte, 20)
+	//Get the filesize
+	connection.Read(bufferFileSize)
+	//Strip the ':' from the received size, convert it to a int64
+	fileSize, _ := strconv.ParseInt(strings.Trim(string(bufferFileSize), ":"), 20, 64)
+	//Get the filename
+	connection.Read(bufferFileName)
+	//Strip the ':' once again but from the received file name now
+	fileName := strings.Trim(string(bufferFileName), ":")
+	//Create a new file to write in
+	newFile, err := os.Create(fileName)
+	if err != nil {
+		panic(err)
+	}
+	defer newFile.Close()
+	//Create a variable to store in the total amount of data that we received already
+	var receivedBytes int64
+	//Start writing in the file
+	for {
+		if (fileSize - receivedBytes) < BUFFERSIZE {
+			io.CopyN(newFile, connection, (fileSize - receivedBytes))
+			//Empty the remaining bytes that we don't need from the network buffer
+			connection.Read(make([]byte, (receivedBytes+BUFFERSIZE)-fileSize))
+			//We are done writing the file, break out of the loop
+			break
+		}
+		io.CopyN(newFile, connection, BUFFERSIZE)
+		//Increment the counter
+		receivedBytes += BUFFERSIZE
+	}
+	fmt.Println("Received file completely!")
+}
--- listenerserver.go
+++ listenerserver.go
@@ -1,171 +1,171 @@
 package main
 
 import (
-    "fmt"
-    "net"
-    "strconv"
-    "os"
-    "strings"
-    "bufio"
-    "encoding/json"
-	  "path/filepath"
-	  "crypto/sha1"
-	  "encoding/hex"
-	  "log"
-    "time"
-    "net/http"
-	  "io"
+	"bufio"
+	"crypto/sha1"
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
+	"time"
 )
+
 //Define the size of how big the chunks of data will be send each time
 const BUFFERSIZE = 81920
 
 const (
-    CONN_HOST = "0.0.0.0"
-    CONN_PORT = "3333"
-    CONN_TYPE = "tcp"
+	CONN_HOST = "0.0.0.0"
+	CONN_PORT = "3333"
+	CONN_TYPE = "tcp"
 )
 
 type File_s struct {
 	Name string
 	Hash string
-	Size int64	
-  path string
+	Size int64
+	path string
 }
 
-var files []File_s 
+var files []File_s
 var INSERT_URL = "http://ayushpateria.com/ShareIIT/insert.php"
 var myIP string
 
-
 // Get preferred outbound ip of this machine. Taken from http://stackoverflow.com/a/37382208/921872
 func GetOutboundIP() string {
-    conn, err := net.Dial("udp", "8.8.8.8:80")
-    if err != nil {
-        log.Fatal(err)
-    }
-    defer conn.Close()
+	conn, err := net.Dial("udp", "8.8.8.8:80")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
 
-    localAddr := conn.LocalAddr().String()
-    idx := strings.LastIndex(localAddr, ":")
-    return localAddr[0:idx]
+	localAddr := conn.LocalAddr().String()
+	idx := strings.LastIndex(localAddr, ":")
+	return localAddr[0:idx]
 }
-
 
 func sendIP() {
-  response, err := http.Get(INSERT_URL+"?IP="+GetOutboundIP())
-        if err != nil {
-                fmt.Println(err)
-        } else {
-                defer response.Body.Close()
-                if err != nil {
-                        fmt.Println(err)
-                }
-        }
+	response, err := http.Get(INSERT_URL + "?IP=" + GetOutboundIP())
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		defer response.Body.Close()
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
 }
+
 func main() {
 
-sendIP()
+	sendIP()
 
-quit := make(chan struct{})
+	quit := make(chan struct{})
 
-ticker := time.NewTicker(300 * time.Second)
+	ticker := time.NewTicker(300 * time.Second)
 
-go func() {
-    for {
-       select {
-        case <- ticker.C:
-            sendIP()
-        case <- quit:
-            ticker.Stop()
-            return
-        }
-    }
- }()
-    // Listen for incoming connections.
-    l, err := net.Listen(CONN_TYPE, CONN_HOST+":"+CONN_PORT)
-    if err != nil {
-        fmt.Println("Error listening:", err.Error())
-        os.Exit(1)
-    }
-    // Close the listener when the application closes.
-    defer l.Close()
-    fmt.Println("Listening on " + CONN_HOST + ":" + CONN_PORT)
-    for {
-        // Listen for an incoming connection.
-        conn, err := l.Accept()
-        if err != nil {
-            fmt.Println("Error accepting: ", err.Error())
-            os.Exit(1)
-        }
-        // Handle connections in a new goroutine.
-        go handleRequest(conn)
-    }
+	go func() {
+		for {
+			select {
+			case <-ticker.C:
+				sendIP()
+			case <-quit:
+				ticker.Stop()
+				return
+			}
+		}
+	}()
+	// Listen for incoming connections.
+	l, err := net.Listen(CONN_TYPE, CONN_HOST+":"+CONN_PORT)
+	if err != nil {
+		fmt.Println("Error listening:", err.Error())
+		os.Exit(1)
+	}
+	// Close the listener when the application closes.
+	defer l.Close()
+	fmt.Println("Listening on " + CONN_HOST + ":" + CONN_PORT)
+	for {
+		// Listen for an incoming connection.
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting: ", err.Error())
+			os.Exit(1)
+		}
+		// Handle connections in a new goroutine.
+		go handleRequest(conn)
+	}
 }
 
 // Handles incoming requests.
 func handleRequest(conn net.Conn) {
-  // Make a buffer to hold incoming data.
+	// Make a buffer to hold incoming data.
 
-  // Read the incoming connection into the buffer.
-  choice, err := bufio.NewReader(conn).ReadString('\n')
-  if err != nil {
-    fmt.Println("Error reading:", err.Error())
-  }
-  if choice[:1] == "1" {
-            //conn.Write([]byte("im in 1 \n"))
-            b := list()
-            conn.Write([]byte(b+"\n"))
-            conn.Close()
-            //conn.Write([]byte(list()))
-             
-  }
-  if choice[:1] == "2" { 
-            hash := choice[2:len(choice)-1]
-              
-             for _, f := range files {
-               if (f.Hash == hash) {
-                   sendFileToClient(conn, f.path)
-              }
-            } 
-  }
-   
+	// Read the incoming connection into the buffer.
+	choice, err := bufio.NewReader(conn).ReadString('\n')
+	if err != nil {
+		fmt.Println("Error reading:", err.Error())
+	}
+	if choice[:1] == "1" {
+		//conn.Write([]byte("im in 1 \n"))
+		b := list()
+		conn.Write([]byte(b + "\n"))
+		conn.Close()
+		//conn.Write([]byte(list()))
+
+	}
+	if choice[:1] == "2" {
+		hash := choice[2 : len(choice)-1]
+
+		for _, f := range files {
+			if f.Hash == hash {
+				sendFileToClient(conn, f.path)
+			}
+		}
+	}
+
 }
 
 //This function is to 'fill'
 func fillString(retunString string, toLength int) string {
-  for {
-    lengtString := len(retunString)
-    if lengtString < toLength {
-      retunString = retunString + ":"
-      continue
-    }
-    break
-  }
-  return retunString
+	for {
+		lengtString := len(retunString)
+		if lengtString < toLength {
+			retunString = retunString + ":"
+			continue
+		}
+		break
+	}
+	return retunString
 }
 
-func list() string{
-  files = nil
+func list() string {
+	files = nil
 	filepath.Walk("Shared", func(path string, info os.FileInfo, err error) error {
 		//fmt.Println(path)
-			file, err := os.Open(path)
-			fileInfo, err := file.Stat()
-			if err != nil {
-				log.Fatal(err)
-			}
+		file, err := os.Open(path)
+		fileInfo, err := file.Stat()
+		if err != nil {
+			log.Fatal(err)
+		}
 
-			if(fileInfo.Size() != 0 && !fileInfo.IsDir()){
-				f := File_s{}
-				f.Name = fileInfo.Name()
-				f.Size = fileInfo.Size()
-        f.path = path
-				hash,_ := hash_file_sha1(f)
-				f.Hash = hash
-				files = append(files, f)
-			}
- 		return nil
+		if fileInfo.Size() != 0 && !fileInfo.IsDir() {
+			f := File_s{}
+			f.Name = fileInfo.Name()
+			f.Size = fileInfo.Size()
+			f.path = path
+			hash, _ := hash_file_sha1(f)
+			f.Hash = hash
+			files = append(files, f)
+		}
+		return nil
 	})
-  
+
 	b, err := json.Marshal(files)
 	if err != nil {
 		fmt.Println("error:", err)
@@ -177,70 +177,70 @@ func list() string{
 func hash_file_sha1(f File_s) (string, error) {
 	//Initialize variable returnMD5String now in case an error has to be returned
 	var returnSHA1String string
-	
+
 	//Open the filepath passed by the argument and check for any error
 	file, err := os.Open(f.path)
 	if err != nil {
 		return returnSHA1String, err
 	}
-  // Read first 20 bytes.
-  b1 := make([]byte, 20)
-  file.Read(b1)
+	// Read first 20 bytes.
+	b1 := make([]byte, 20)
+	file.Read(b1)
 	ss := f.Name + string(f.Size) + string(b1)
 	//Tell the program to call the following function when the current function returns
 	defer file.Close()
-	
+
 	//Open a new SHA1 hash interface to write to
 	hash := sha1.New()
-	
+
 	hash.Write([]byte(ss))
 	//Get the 20 bytes hash
 	hashInBytes := hash.Sum(nil)[:20]
-	
+
 	//Convert the bytes to a string
 	returnSHA1String = hex.EncodeToString(hashInBytes)
-	
+
 	return returnSHA1String, nil
- 
+
 }
 
 func sendFileToClient(connection net.Conn, path string) {
-  fmt.Println("A client has connected!")
-  //defer connection.Close()
-  //Open the file that needs to be send to the client
-  file, err := os.Open(path)                      //pass a hash ka return string i.e -filename in that sharedfolder
-  if err != nil {
-    fmt.Println(err)
-    return
-  }
-  //Get the filename and filesize
-  fileInfo, err := file.Stat()
-  if err != nil {
-    fmt.Println(err)
-    return
-  }
-  fileSize := fillString(strconv.FormatInt(fileInfo.Size(), 20), 20)
-  fileName := fillString(fileInfo.Name(), 128)
-  fmt.Println(string(fileName))
-      fmt.Println(string(fileSize))
-                //Send the file header first so the client knows the filename and how long it has to read the incomming file
-  fmt.Println("Sending filename and filesize!")
-                                                    //Write first 10 bytes to client telling them the filesize
-  connection.Write([]byte(fileSize))
-                                                    //Write 64 bytes to client containing the filename
-  connection.Write([]byte(fileName))
-                                                  //Initialize a buffer for reading parts of the file in
-  sendBuffer := make([]byte, BUFFERSIZE)
-                                                  //Start sending the file to the client
-  fmt.Println("Start sending file!")
-  for {
-    _, err = file.Read(sendBuffer)
-    if err == io.EOF {
-      //End of file reached, break out of for loop
-      break
-    }
-    connection.Write(sendBuffer)
-  }
-  fmt.Println("File has been sent, closing connection!")
-  return
+	fmt.Println("A client has connected!")
+	//defer connection.Close()
+	//Open the file that needs to be send to the client
+	file, err := os.Open(path) //pass a hash ka return string i.e -filename in that sharedfolder
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	//Get the filename and filesize
+	fileInfo, err := file.Stat()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fileSize := fillString(strconv.FormatInt(fileInfo.Size(), 20), 20)
+	fileName := fillString(fileInfo.Name(), 128)
+	fmt.Println(string(fileName))
+	fmt.Println(string(fileSize))
+	//Send the file header first so the client knows the filename and how long it has to read the incomming file
+	fmt.Println("Sending filename and filesize!")
+	//Write first 10 bytes to client telling them the filesize
+	connection.Write([]byte(fileSize))
+	//Write 64 bytes to client containing the filename
+	connection.Write([]byte(fileName))
+	//Initialize a buffer for reading parts of the file in
+	sendBuffer := make([]byte, BUFFERSIZE)
+	//Start sending the file to the client
+	fmt.Println("Start sending file!")
+	for {
+		_, err = file.Read(sendBuffer)
+		if err == io.EOF {
+			//End of file reached, break out of for loop
+			break
+		}
+		connection.Write(sendBuffer)
+	}
+	fmt.Println("File has been sent, closing connection!")
+	return
 }
